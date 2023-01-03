78f8444337573c4707d7d09c956503daba015a56
arthurrogado <arthurrogadoreis@hotmail.com>
2023-01-03 20:41:49 -0300

    registro usuario

--- termo_bot.py
+++ termo_bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, MenuButtonWebApp, User
 from config import token
 import sqlite3
-import datetime
+import datetime as dt
 
 banco = sqlite3.connect('database.db', check_same_thread=False)
 c = banco.cursor()
@@ -25,7 +25,6 @@ markup_termo = InlineKeyboardMarkup(
 
 def verificar_se_existe_usuario(User):
     userid = User.id
-    print('iniciando teste')
     c.execute(f"SELECT * FROM usuarios WHERE id = '{userid}'")
     usuarios = c.fetchall()
     if usuarios:
@@ -34,29 +33,25 @@ def verificar_se_existe_usuario(User):
         return False
 
 def inserir_usuario(User):
-    userid = User.id
-    if verificar_se_existe_usuario(userid):
-        c.execute(f"INSERT INTO usuario (id, nome, ultimo_jogo) VALUES ({userid}, {User.first_name}, {datetime.datetime.now('America/Sao_Paulo')})")
+    if verificar_se_existe_usuario(User):
+        return False
+    else:
+        print(f"INSERT INTO usuarios (id, nome, data_registro) VALUES ({User.id}, {User.first_name}, '{ dt.datetime.now() }')")
+        c.execute(f"INSERT INTO usuarios (id, nome, data_registro) VALUES ('{User.id}', '{User.first_name}', '{ dt.datetime.now() }')")
         banco.commit()
         return True
-    else:
-        return False
-
-def atualizar_ultimo_jogo(userid):
-    if verificar_se_existe_usuario(userid):
-        if c.execute(f'UPDATE usuarios WHERE id = {userid} SET ultimo_jogo = {datetime.datetime.now("America/Sao_Paulo")}'):
-            banco.commit()
-            return True
-    return False
 
 
 def start_bot():
+
     pass
 
 ### HANDLER PARA START ###
 @bot.message_handler(commands=['start'])
 def start(msg):
-    userid = msg.chat.id
+    User = msg.chat
+    inserir_usuario(User)
+    userid = User.id
     menu = MenuButtonWebApp('web_app', text="Abrir TERMO 🔠" , web_app =  WebAppInfo(url=url_termo))
     bot.set_chat_menu_button(userid, menu)
     bot.send_message(userid, 'Clique no botão abaixo para abrir o app!')
